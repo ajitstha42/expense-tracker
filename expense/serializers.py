f908c1511cf42b4c2f8902e2d24758e30677f1de
Ajit Shrestha <ajitstha16@gmail.com>
2025-07-04 21:48:59 +0545

    to get specific expense

--- expense/serializers.py
+++ expense/serializers.py
@@ -9,3 +9,14 @@ class ExpenseIncomeBaseSerializer(serializers.ModelSerializer):
         if obj.tax_type == "percentage":
             return round(obj.amount + (obj.amount * obj.tax / 100), 2)
         return round(obj.amount + obj.tax, 2)
+
+
+class ExpenseIncomeSerializer(ExpenseIncomeBaseSerializer):
+    total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ExpenseIncome
+        fields = ["id", "title", "description", "amount", "transaction_type", "tax", "tax_type", "total", "created_at", "updated_at",]
+        read_only_fields = ["id", "created_at", "updated_at", "total"]
+
+
