efa7895523051eedc83025dbacfa3a4c2f057d0d
Ajit Shrestha <ajitstha16@gmail.com>
2025-07-04 21:55:15 +0545

    creation of expense income views

--- expense/views.py
+++ expense/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ExpenseIncome
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ExpenseIncomeSerializer, ExpenseIncomeListSerializer
+from .permissions import IsOwnerOrSuperuser
 
-# Create your views here.
+
+class ExpenseIncomeViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsOwnerOrSuperuser]
+    queryset = ExpenseIncome
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ExpenseIncomeListSerializer
+        return ExpenseIncomeSerializer
